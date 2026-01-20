9dfa6a91038d1134052d1e37af0a824d2967ef3d
Neia <neiabaraja7572000@gmail.com>
2026-01-20 20:22:26 -0300

    se agrego la funcion de sumar

--- calcu.py
+++ calcu.py
@@ -1,2 +1,6 @@
+def appSumar(num1, num2):
+    resultado = num1 + num2
+    return resultado
 n1 = int(input("Ingresa primer numero: "))
 n2 = int(input("Ingresa segundo numero: "))
+print(appSumar(n1, n2))
