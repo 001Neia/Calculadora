70f2d7c68e42fcb30183a35d26ebc2bb74d68f19
Neia <neiabaraja7572000@gmail.com>
2026-01-21 16:45:25 -0300

    se agrego una resta

--- calcu.py
+++ calcu.py
@@ -1,6 +1,10 @@
 def appSumar(num1, num2):
     resultado = num1 + num2
     return resultado
+def aplicacionParaRestar(numero1, numero2):
+    ejecucion = numero1 - numero2
+    return ejecucion
 n1 = int(input("Ingresa primer numero: "))
 n2 = int(input("Ingresa segundo numero: "))
 print(appSumar(n1, n2))
+print(aplicacionParaRestar(n1, n2))
