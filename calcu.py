171d1b975184f94bbbdbc0228608cd7b97f16fc4
Neia <neiabaraja7572000@gmail.com>
2026-01-21 17:49:42 -0300

    se agrego multiplicar

--- calcu.py
+++ calcu.py
@@ -4,7 +4,11 @@ def appSumar(num1, num2):
 def apprestar(num1, num2):
     resultado = num1 - num2
     return resultado
+def appMultiplicar(num1, num2):
+    resultado = num1 * num2
+    return resultado
 n1 = int(input("Ingresa primer numero: "))
 n2 = int(input("Ingresa segundo numero: "))
 print(appSumar(n1, n2))
 print (apprestar(n1, n2))
+print(appMultiplicar(n1, n2))
