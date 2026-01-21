1ab410a815d5a5003036ae01c9fd10d4453a10fb
Neia <neiabaraja7572000@gmail.com>
2026-01-21 16:43:39 -0300

    se agrego la funcion restar

--- calcu.py
+++ calcu.py
@@ -1,6 +1,10 @@
 def appSumar(num1, num2):
     resultado = num1 + num2
     return resultado
+def apprestar(num1, num2):
+    resultado = num1 - num2
+    return resultado
 n1 = int(input("Ingresa primer numero: "))
 n2 = int(input("Ingresa segundo numero: "))
 print(appSumar(n1, n2))
+print (apprestar(n1, n2))
